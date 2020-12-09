0bf78c67966a9a04ef30ffd2fb58d5b00b633d5f
kokonattsu <759295026@qq.com>
2020-12-09 11:10:26 +0800

    build(account): mapper接口、pojo、xml

--- seata-samples/seate-at/account/src/main/java/cn/tedu/account/AccountApplication.java
+++ seata-samples/seate-at/account/src/main/java/cn/tedu/account/AccountApplication.java
@@ -1,8 +1,9 @@
 package cn.tedu.account;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("cn.tedu.account.mapper")
 @SpringBootApplication
 public class AccountApplication {
 
