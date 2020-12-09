7d87b6834a6b9a8bf09786d928b3fea54d444d75
kokonattsu <759295026@qq.com>
2020-12-09 11:16:07 +0800

    test(config): 配置中心改成ip显示，未完成。

--- springcloud-02/sp12-config/src/test/java/cn/tedu/sp12/Sp12ConfigApplicationTests.java
+++ springcloud-02/sp12-config/src/test/java/cn/tedu/sp12/Sp12ConfigApplicationTests.java
@@ -3,11 +3,29 @@ package cn.tedu.sp12;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import java.util.Arrays;
+
+//@SpringBootTest
 class Sp12ConfigApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() {//冒泡排序
+        int[] a={7,9,4,6,8,3,4,2,8};
+        for (int i=1;i<a.length;i++){
+            for (int j=0;j<a.length-i;j++){
+                if (a[j]>a[j+1]){
+                    int b=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=b;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(a));
     }
 
+    @Test
+    void test2(){
+        
+    }
 }
