e4191729a19aa1405c3856ec7e86d88dd69c1aa3
kokonattsu <759295026@qq.com>
2020-12-09 20:00:27 +0800

    feature(order): 使用feign减少账户余额和减少库存

--- seata-samples/seate-at/order/src/main/java/cn/tedu/order/OrderApplication.java
+++ seata-samples/seate-at/order/src/main/java/cn/tedu/order/OrderApplication.java
@@ -3,7 +3,10 @@ package cn.tedu.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 @MapperScan("cn.tedu.order.mapper")
+@EnableFeignClients
 @SpringBootApplication
 public class OrderApplication {
 
--- seata-samples/seate-at/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
+++ seata-samples/seate-at/order/src/main/java/cn/tedu/order/service/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package cn.tedu.order.service;
 
 import cn.tedu.order.entity.Order;
+import cn.tedu.order.feign.AccountClient;
+import cn.tedu.order.feign.EasyIdGeneratorClient;
+import cn.tedu.order.feign.StorageClient;
 import cn.tedu.order.mapper.OrderMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,15 +16,29 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private EasyIdGeneratorClient easyIdGeneratorClient;
+    @Autowired
+    private StorageClient storageClient;
+    @Autowired
+    private AccountClient accountClient;
 
     @Override
     public void create(Order order) {
-        //TODO:从全局唯一发号器获得id
-        Long orderId=Math.abs(new Random().nextLong());
+        //从全局唯一发号器获得id   order_business
+        Long orderId =Long.valueOf(easyIdGeneratorClient.
+                nextId("order_business"));
+        //Long orderId=Math.abs(new Random().nextLong());
         order.setId(orderId);
         orderMapper.create(order);
-        log.info("创建订单:"+order.getId());
-        //TODO:减少商品库存
-        //TODO:减少账户余额
+        //减少商品库存
+        storageClient.decrease(
+                order.getProductId(),
+                order.getCount());
+        //减少账户余额
+        accountClient.decrease(
+                order.getUserId(),
+                order.getMoney());
+        log.info("订单:"+orderId+"创建完毕");
     }
 }
